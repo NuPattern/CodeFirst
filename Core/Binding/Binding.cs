a563f3674ba0ee5361bc2bda33cc35dda47e61be
Daniel Cazzulino <daniel@cazzulino.com>
2013-09-27 21:38:18 -0300

    Optimize execution path for constant property bindings, so that they are evaluated only once for a binding. #48

--- Core/Binding/Binding.cs
+++ Core/Binding/Binding.cs
@@ -19,19 +19,40 @@
 
     public class Binding : IBinding
     {
+        private PropertyBinding[] allProperties;
+        private PropertyBinding[] dynamicProperties;
+
+        private bool initialized;
+
         public Binding(object instance, IEnumerable<PropertyBinding> properties)
         {
             this.Instance = instance;
-            this.Properties = properties.ToArray();
+            this.allProperties = properties.ToArray();
         }
 
         public object Instance { get; private set; }
 
-        public IEnumerable<PropertyBinding> Properties { get; private set; }
+        public IEnumerable<PropertyBinding> Properties { get { return allProperties; } }
 
         public void Refresh()
         {
-            foreach (var property in Properties)
+            if (!initialized)
+            {
+                // First time we initialize, we refresh the constant properties. 
+                // It's the only time constant properties are evaluated, since they 
+                // can't change later.
+                foreach (var constantProperty in allProperties.OfType<ConstantPropertyBinding>())
+                {
+                    constantProperty.Refresh(Instance);
+                }
+
+                // We separate dynamic properties, which are used for refreshing.
+                dynamicProperties = allProperties.OfType<ProvidedPropertyBinding>().ToArray();
+                initialized = true;
+            }
+
+            // Note: only dynamic properties are refreshed subsequently.
+            foreach (var property in dynamicProperties)
             {
                 property.Refresh(Instance);
             }
